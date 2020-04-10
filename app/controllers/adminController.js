2cb6ace5d1bcf4c8d1483d2bf8461bcea58827f5
marccharton <kramoogle@gmail.com>
2020-04-10 17:34:42 +0200

    feat: add new exercice

--- app/controllers/adminController.js
+++ app/controllers/adminController.js
@@ -1,14 +1,50 @@
+const { Exercice } = require("../models");
+
 const adminController = {
+
+    viewParam: {
+        isAdminMode: true
+    },
+
     homePage: (req, res) => {
         res.redirect("/admin/exercice");
     },
     
     addExercicePage: (req, res) => {
-        res.render("admin", {isAdminMode:true});
+        res.render("admin", {isAdminMode: true});
     },
 
-    addExerciceAction: (req, res) => {
-        return res.send(req.body);
+    addExerciceAction: async (req, res) => {
+        
+        try {
+            const exercice = await Exercice.findOne({
+                where: {
+                    name: req.body.name
+                }
+            });
+
+            if (exercice) {
+                return res.render("admin", {
+                    ...this.viewParam, 
+                    error: "Un exercice avec le même nom existe déjà",
+                });
+            }
+            
+            let newExercice = new Exercice({
+                name: req.body.name,
+                description: req.body.description,
+                correction: req.body.correction,
+            });
+            await newExercice.save();
+    
+            res.render("admin", { 
+                ...this.viewParam, 
+                information: "Exercice enregistré :)" 
+            });
+
+        } catch (error) {
+            res.render("error", { error });
+        }
     },
 };
 
