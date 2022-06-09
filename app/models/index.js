20b84982803139fcc42fc3a63cdcc8bacf3738ff
marccharton <kramoogle@gmail.com>
2022-06-09 21:25:55 +0200

    fix: can't find exercice module

--- app/models/index.js
+++ app/models/index.js
@@ -1,7 +1,7 @@
 // SoC: Le rôle de ce fichier est de définir les associations entre les différents models !
 
 // 1. D'abord, require tous nos modèles
-const Exercice = require('./exercice');
+const Exercice = require('./Exercice');
 const User = require('./user');
 
 // 2. définir toutes les associations
